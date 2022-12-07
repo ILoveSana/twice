83e1cd28070c8306a8e7907e31209eb36763f2cb
jamkaman <59901740+jamkaman@users.noreply.github.com>
2022-12-07 22:44:47 +0000

    asdasd

--- twice.java
+++ twice.java
@@ -1,1 +1,24 @@
-syste
+import javax.sound.sampled.*;
+import java.io.*;
+import java.net.URL;
+public class twice{
+    public static synchronized void playSound(final String url) {
+        new Thread(new Runnable() {
+        // The wrapper thread is unnecessary, unless it blocks on the
+        // Clip finishing; see comments.
+          public void run() {
+            try {
+              Clip clip = AudioSystem.getClip();
+              InputStream input = new URL(url).openStream();
+              AudioInputStream inputStream = AudioSystem.getAudioInputStream(input);
+              
+              clip.open(inputStream);
+              clip.start(); 
+            } catch (Exception e) {
+              System.err.println(e.getMessage());
+            }
+          }
+        }).start();
+      }
+}
+
